c4a2d76724aca5f7f97758aa3cd1fe4cf4ab8352
tjddhd <tjddhd@mst.edu>
2019-10-21 06:30:37 -0500

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace AzureStatsComm.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, msg);
+        }
+    }
+}
